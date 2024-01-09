3b77c82d1962d12d970a3051a0819a5057f37fa2
Naomi Lima <naomilima00@gmail.com>
2024-01-08 21:07:20 -0500

    Initial setup
    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/**/*.{html,ts}'],
+  theme: {
+    colors: {
+      'cod-gray': '#2A2F32',
+      'jupiter': '#E1E1E1'
+    }
+  },
+  plugins: [],
+}
+
